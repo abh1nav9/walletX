3e77f8c5f962fb3f7e338abc7b6be864693096a5
Abhinav Gautam <ABHINAVGAUTAM.210111063@gehu.ac.in>
2024-09-05 16:38:55 +0530

    Added Bank Account Schema

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,9 +4,12 @@ const { authMiddleware } = require("../middlewares/auth");
 
 const router = express.Router();
 
+// User Routes
 router.get("/bulk", getAllUsers);
 router.post("/signup", userSignup);
 router.post("/login", userLogin);
 router.put("/", authMiddleware, userModifyData);
+
+//Account Routes
 
 module.exports = router;
