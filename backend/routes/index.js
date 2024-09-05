878c9522a332be985525c9a94a3c7d057c5312f6
Abhinav Gautam <ABHINAVGAUTAM.210111063@gehu.ac.in>
2024-09-05 16:17:46 +0530

    Fixed User Routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const {userSignup, userLogin, userModifyData} = require("../controllers/user");
+const {userSignup, userLogin, userModifyData, getAllUsers} = require("../controllers/user");
+const { authMiddleware } = require("../middlewares/auth");
 
 const router = express.Router();
 
+router.get("/bulk", getAllUsers);
 router.post("/signup", userSignup);
 router.post("/login", userLogin);
+router.put("/", authMiddleware, userModifyData);
 
 module.exports = router;
